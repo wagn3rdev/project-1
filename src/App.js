2855462cfdd6702d4341a1bbc537baaa37abacd7
wagner <wagncarv@outlook.com>
2023-08-30 13:48:22 -0300

    props, components, parte 1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import { Component } from 'react';
+import { PostCard } from './components/PostCard';
 
 class App extends Component {
   state = {
@@ -50,13 +51,7 @@ class App extends Component {
       <section className='container'>
         <div className='posts'>
           {posts.map(post => (
-            <div className='post'>
-              <img src={post.cover} alt={post.title} />
-              <div key={post.id} className='post-content'>
-                <h1>{post.title}</h1>
-                <p>{post.body}</p>
-              </div>
-            </div>
+            <PostCard key={post.id} title={post.title} id={post.id} cover={post.cover} body={post.body} />
           ))}
         </div>
       </section>
